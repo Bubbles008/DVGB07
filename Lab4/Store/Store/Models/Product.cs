3710f4cd62e9efd6be43fdf0ffd9f989bfce8b24
Helin Camila Kalak <helinkalak1@gmail.com>
2026-04-18 16:17:03 +0200

    Set up classes

---
+++ Lab4/Store/Store/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int QuantityInStock { get; set; }
+
+        public Product
+}
