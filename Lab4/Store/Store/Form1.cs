2a6e959d9e753c5252ced73cb4a057cc00889f5f
Helin Camila Kalak <helinkalak1@gmail.com>
2026-04-22 19:48:19 +0200

    .

--- Lab4/Store/Store/Form1.cs
+++ Lab4/Store/Store/Form1.cs
@@ -114,11 +114,13 @@ namespace Store
 
            
         }
+        }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
